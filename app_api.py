b66aa35395ed079fd75b1f9be5dfcb34f99ba6a6
fcamara30-byte <fcamara30@gmail.com>
2026-05-15 20:13:09 -0300

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -92,11 +92,13 @@ f_base=CO2[co2]*H2S[h2s]*BSR[bsr]*f_cl(cl)
 
 def FS_material(mat,f):
     if f==1: return 1
-    if mat=="HS97": return f
     if mat=="DA78": return f*0.95
-    if mat=="DSK75": return f if f<0.83 else 1
-    if mat=="HA96": return f*0.93
-    if mat=="D New": return f*0.94
+    elif mat=="HS97": return f*0.96
+    elif mat=="CS propietario": return f*0.96
+    elif mat=="HS propietario": return f*0.75
+    elif mat=="D New": return f*0.94
+    elif mat=="DSK75": return f if f < 0.75 else 1
+    elif mat=="HA96": return f*0.85
     return f*0.9
 
 # ======================
