a4415d75913f1d9d15d9cf98d58331c005a3642f
fcamara30-byte <fcamara30@gmail.com>
2026-05-17 18:08:55 -0300

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -455,5 +455,5 @@ with col_plot:
 
 st.markdown("---")
 st.caption("Basada en cálculos APIRP11L, Estudios de Corrosión-Fatiga y experiencias de Campo.")
-st.markdown('<div class="cursiva">Desarrollado por Fcam & Eng.Pro. SP-Brazil May-26</div>', unsafe_allow_html=True)
+st.markdown('<div class="cursiva">Desarrollado por Fcam - Eng.Pro. SP-Brazil May-26</div>', unsafe_allow_html=True)
 
