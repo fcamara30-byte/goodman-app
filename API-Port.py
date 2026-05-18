177a02dfad5f1d7273e2951aae1b3234f0a8e593
fcamara30-byte <fcamara30@gmail.com>
2026-05-18 11:13:34 -0300

    Update API-Port.py

--- API-Port.py
+++ API-Port.py
@@ -42,10 +42,6 @@ col_title, col_img = st.columns([5,1])
 with col_title:
     st.title("Cálculo das solicitações SRP por corrosão-fadiga")
 
-with col_flag:
-    st.markdown(
-        "<div style='font-size:30px; text-align:right;'>🇧🇷</div>",
-        unsafe_allow_html=True)
 
 with col_img:
     st.markdown(
