ea85ae55842285534b59aa238d2487464deb9dc7
fcamara30-byte <fcamara30@gmail.com>
2026-05-18 11:44:01 -0300

    Update API-Port.py

--- API-Port.py
+++ API-Port.py
@@ -120,7 +120,7 @@ BSR={"0":1,"1":1,"2":0.95,"3":0.9,"4":0.82,"5":0.74}
 col1, col2, col3, col4, _ = st.columns([2,2,2,2,2])  # ← mismo criterio compacto
 
 with col1:
-    co2 = st.selectbox("CO₂", CO2)
+    co2 = st.selectbox("PPCO₂", CO2)
 
 with col2:
     h2s = st.selectbox("H₂S", H2S)
