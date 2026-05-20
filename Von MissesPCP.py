7ee4c170f9061e1604495b93bf97b508506f9bad
fcamara30-byte <fcamara30@gmail.com>
2026-05-20 16:51:47 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -57,7 +57,7 @@ with colL:
     with c1:
         nombre_pozo = st.text_input("Nombre Pozo")
         profundidad = st.number_input("Profundidad (m)",600,step=100)
-        rpm = st.number_input("RPM (rev/min)",0,step=20)
+        rpm = st.number_input("RPM (rev/min)",1,step=20)
         prod = st.number_input("Producción (m³/d)",150.0)
         pres_linea = st.number_input("Presión línea (kg/cm²)", value=1.0,    step=2.0
 )
