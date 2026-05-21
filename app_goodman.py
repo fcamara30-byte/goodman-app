3f4ecccff4bde57e466b2790fe178d06e2135ff2
fcamara30-byte <fcamara30@gmail.com>
2026-05-21 00:10:11 -0300

    Update app_goodman.py

--- app_goodman.py
+++ app_goodman.py
@@ -58,7 +58,7 @@ st.caption(f"Visitas totales: {visitas}")
 # DATOS
 # ======================
 materiales = {
-    "DA78":{"uts_a":30,"b":0.5625},
+    "DA78":{"uts_a":42.86,"b":0.375},
     "HS97":{"uts_a":50,"b":0.375},
     "CS propietario":{"uts_a":44.64,"b":0.375},
     "HS propietario":{"uts_a":55.36,"b":0.375},
@@ -101,7 +101,7 @@ def FS_material(mat,f):
     elif mat=="CS propietario": return f*0.955
     elif mat=="HS propietario": return f*0.75
     elif mat=="D New": return f*0.90
-    elif mat=="DSX75": return f if f < 0.75 else 1
+    elif mat=="DSX75": return f if f < 0.73 else 1
     elif mat=="HA96": return f*0.8
 
 def goodman(x,uts,b,fs):
