acd2bef4b968ec0065d574f86e35ef82643d4fbd
fcamara30-byte <fcamara30@gmail.com>
2026-05-21 00:15:53 -0300

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -135,7 +135,7 @@ def FS_material(mat,f):
     elif mat=="CS propietario": return f*0.92
     elif mat=="HS propietario": return f*0.75
     elif mat=="D New": return f*0.90
-    elif mat=="DSX75": return f if f < 0.75 else 1
+    elif mat=="DSX75": return f if f < 0.73 else 1
     elif mat=="HA96": return f*0.85
     return f*0.9
 
