f50d4e0bd89eb2722a046c37ecc66aff05ed0b95
fcamara30-byte <fcamara30@gmail.com>
2026-05-22 01:23:44 -0300

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -261,9 +261,8 @@ with col_tabla:
 
 # ✅ ALERTA
 if abs(dif) > 20:
- st.markdown("""
-    <style>
-    @keyframes blink {
+     st.markdown("""     <style>
+     @keyframes blink {
         0% {opacity: 1;}
         50% {opacity: 0;}
         100% {opacity: 1;}
