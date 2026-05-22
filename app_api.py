15889839373f734ab68317a537f78d85e84f9584
fcamara30-byte <fcamara30@gmail.com>
2026-05-22 18:37:42 -0300

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -85,7 +85,7 @@ with col3:
         padding:6px 8px;
         height:52px;
         display:flex;
-        margin-top:-40px;
+        margin-top:-60px;
         flex-direction:column;
         justify-content:center;
         align-items:center;
