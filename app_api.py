e1e71287b0d1b0d3e35f89777f3e602d799ffffd
fcamara30-byte <fcamara30@gmail.com>
2026-05-22 19:27:49 -0300

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -288,7 +288,7 @@ dF=min(dF,limite)
 
 MPRL_base=max(Wr-dF,0)
 MPRL = MPRL_base *0.85
-HP =(Q_bpd * ((PPRL + MPRL) / 2) / (52900 )
+HP =(Q_bpd * ((PPRL + MPRL) / 2) / (52900 ))
 
 # ======================
 # DISPLAY
