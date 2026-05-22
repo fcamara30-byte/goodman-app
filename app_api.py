213d4125543ec7578d4f2cf635be142727ff03a2
fcamara30-byte <fcamara30@gmail.com>
2026-05-22 02:03:34 -0300

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -962,7 +962,7 @@ F1_Skr = F2_Skr * 1.15
 
 # ✅ cargas
 PPRL = Wr + (F1_Skr * S * kr)
-MPRL = (Wr - (F2_Skr * S * kr))* 0.7
+MPRL = (Wr - (F2_Skr * S * kr))
 
 
 # --- DEBUG TEMPORAL ---# ---st.write("Fo/Skr:", round(Fo_Skr,3))
