a03d072906966452ad261191b459e80e0f3d44c7
fcamara30-byte <fcamara30@gmail.com>
2026-05-24 00:45:43 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -893,7 +893,8 @@ fig.update_layout(
 
 
 scene=dict(
-    aspectmode='data',
+    aspectmode='manual',
+    aspectratio=dict(x=1, y=1, z=2.5),
 
     camera=dict(
         eye=dict(x=6, y=0.6, z=0.8)   # ✅ MÁS DE COSTADO
