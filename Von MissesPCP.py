862b064af0b822b173de29c6b2c9818efc45b972
fcamara30-byte <fcamara30@gmail.com>
2026-05-24 02:36:24 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -911,7 +911,7 @@ if len(df) > 1:
         updatemenus=[{
             "type":"buttons",
             "x":0.35,
-            "y":0.2,
+            "y":0.35,
             "buttons":[
                 dict(label="▶",
                      method="animate",
