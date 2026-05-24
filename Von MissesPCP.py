819c11bc768834ffef8f19fee5929774be4c9230
fcamara30-byte <fcamara30@gmail.com>
2026-05-24 09:43:09 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -43,6 +43,13 @@ BOMBAS_400_ST = {
 }
 
 import pandas as pd
+
+
+df = pd.DataFrame()   # ✅ SIEMPRE existe
+inc = []
+az = []
+colores = []
+
 import numpy as np
 import matplotlib.pyplot as plt
 import os   # ✅ ESTE FALTABA
