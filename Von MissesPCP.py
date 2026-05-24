147076469d66c7af18748eb91460393a0c36ac28
fcamara30-byte <fcamara30@gmail.com>
2026-05-24 01:23:49 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -927,6 +927,18 @@ if len(df) > 1:
         }]
     )
 
+    
+st.markdown("""
+<style>
+.block-container {
+    padding-top: 0.5rem;
+}
+div[data-testid="stPlotlyChart"] {
+    margin-top: -100px;
+}
+</style>
+""", unsafe_allow_html=True)
+
     st.plotly_chart(fig, use_container_width=True)
 # ===============================
 
