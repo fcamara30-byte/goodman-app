8a1cb1fd4c78714f8b6bc831d765a07fd0f736e8
fcamara30-byte <fcamara30@gmail.com>
2026-05-24 00:51:54 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -896,7 +896,7 @@ scene=dict(
 aspectmode='data',
 
     camera=dict(
-        eye=dict(x=5, y=0.3, z=1.2)   # ✅ MÁS DE COSTADO
+        eye=dict(x=5.5, y=0.2, z=1.6)   # ✅ MÁS DE COSTADO
     ),
 
     zaxis=dict(
