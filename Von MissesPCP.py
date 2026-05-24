5363ef56c359b4186ed3636e853f62c65fe5e4b3
fcamara30-byte <fcamara30@gmail.com>
2026-05-24 10:05:12 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -632,8 +632,11 @@ X = [0]*len(df)
 Y = [0]*len(df)
 Z = [0]*len(df)
 
+
 inc = inc if 'inc' in locals() else [0]
-for i in range(1, len(inc or [0])):
+
+for i in range(1, len(inc)):
+
     dz = df["md"][i] - df["md"][i-1]
 
     X[i] = X[i-1] + np.sin(inc[i]) * np.cos(az[i]) * dz
