0b14087bc4f67b7b5ef87e2df9c789aa32c00a1c
fcamara30-byte <fcamara30@gmail.com>
2026-05-24 04:21:19 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -678,7 +678,7 @@ with colT:
         st.dataframe(
             df[["md","DLS","Recomendación"]]
             .rename(columns={"Recomendación": "Var"}),
-            height=300,
+            height=650,
             use_container_width=True
         )
 
