9c3aef3ee967cb47fdd6db862bb3855274c7d087
fcamara30-byte <fcamara30@gmail.com>
2026-05-24 09:32:07 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -625,8 +625,8 @@ X = [0]*len(df)
 Y = [0]*len(df)
 Z = [0]*len(df)
 
-if modo == "Desviado" and len(df) > 1:
- for i in range(1, len(inc)):
+
+for i in range(1, len(inc)):
     dz = df["md"][i] - df["md"][i-1]
 
     X[i] = X[i-1] + np.sin(inc[i]) * np.cos(az[i]) * dz
