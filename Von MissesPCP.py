cad52b19352da5ae8b1680b4a2b8cb0e76e9a92d
fcamara30-byte <fcamara30@gmail.com>
2026-05-25 11:44:17 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -1110,7 +1110,7 @@ if len(df) > 1:
     escala = max(abs(Xc).max(), abs(Yc).max(), abs(Zc).max())
 
     radio_varilla = escala * 0.002   # 👈 tamaño chico relativo
-    radio_tubo   = radio_varilla * 2.5
+    radio_tubo   = radio_varilla * 3.5
     # ===============================
     # SEMÁFORO
     # ===============================
