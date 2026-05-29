67a02210ade31c5d2f10de39dfd02794fa824354
fcamara30-byte <fcamara30@gmail.com>
2026-05-29 16:37:11 -0300

    Update app_goodman.py

--- app_goodman.py
+++ app_goodman.py
@@ -256,15 +256,21 @@ df["%Goodman"] = ((Smax - Smin) /
 
 col_tabla, col_der = st.columns([2.7,1.8])
 
+col_tabla, col_der = st.columns([2.7,1.8])
+
 with col_tabla:
     st.markdown('<div class="subtitulo">Ranking de Varillas Seleccionadas</div>', unsafe_allow_html=True)
-    "Sadm":"{:.0f}",
-    "Margen":"{:.0f}",
-    "%Goodman":"{:.0f}"
-}),
-use_container_width=False)
-    
-)
+
+    st.dataframe(
+        df.drop(columns=["FS"]).style.format({
+            "Sadm":"{:.0f}",
+            "Margen":"{:.0f}",
+            "%Goodman":"{:.0f}"
+        }),
+        use_container_width=False,
+        height=300
+    )
+
 
 
 with col_der:
