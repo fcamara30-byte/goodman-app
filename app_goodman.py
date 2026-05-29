3dfb1ae02ab23a22160c8deeafc8933b7ea2ac8b
fcamara30-byte <fcamara30@gmail.com>
2026-05-29 16:30:07 -0300

    Update app_goodman.py

--- app_goodman.py
+++ app_goodman.py
@@ -258,7 +258,6 @@ col_tabla, col_der = st.columns([2.7,1.8])
 
 with col_tabla:
     st.markdown('<div class="subtitulo">Ranking de Varillas Seleccionadas</div>', unsafe_allow_html=True)
-st.dataframe(df.drop(columns=["FS"]).style.format({
     "Sadm":"{:.0f}",
     "Margen":"{:.0f}",
     "%Goodman":"{:.0f}"
