e1b0af687b0a2457e819813dbf511f0e55d81ce7
fcamara30-byte <fcamara30@gmail.com>
2026-05-30 00:22:23 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -286,7 +286,6 @@ with colL:
         
 
 
-        Q_cap = Q_teorico * eta_usuario    
 RODS={"7/8":{"d":0.875,"peso":2.22},"1":{"d":1.0,"peso":2.67},"1 1/8":{"d":1.125,"peso":3.37}}
 YIELD={"DA 78":100,"HS97":120,"CS Propietary":110,"HS Propietary":135,"D New":95,"DSX75":110,"HA96":115}
 # =========================
