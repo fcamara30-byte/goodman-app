965d1ecaf803de968c537727ea7c1d68f6908f20
fcamara30-byte <fcamara30@gmail.com>
2026-05-31 22:17:37 -0300

    Update app_api.py

--- app_api.py
+++ app_api.py
@@ -286,7 +286,7 @@ dF = kr * dx * prop_L * (1 + 0.30 * prop_F) * (1 + 1.4 * Fd)
 limite=Wr*(0.45)
 dF=min(dF,limite)
 
-MPRL = max(Wr - Fh - dF * (Fh / Wr), 0)
+MPRL = Wr - 0.2 * dF
 
 
 HP =(L_m * Q_m3 * 0.83 * 0.8) / 2178
