78a07247fae8bc1bda493cbd166b07e02e1ef2b8
fcamara30-byte <fcamara30@gmail.com>
2026-06-03 12:03:43 -0300

    Update app_goodman.py

--- app_goodman.py
+++ app_goodman.py
@@ -408,8 +408,10 @@ if st.session_state.get("show_co2", False):
     with box:
         st.markdown("### Tabla CO₂ (ppm vs P parcial bar)")
 
-        df_co2 = pd.DataFrame({
-            "CO2 (ppm)": [50,100,200,300,500,700,1000,1500,2000,3000],
+df_co2 = pd.DataFrame({
+    "CO2 (ppm)": [50,100,200,300,500,700,1000,1500,2000,3000],
+    "P_CO2 (bar)": [0.03,0.07,0.14,0.21,0.34,0.48,0.69,1.03,1.38,2.07]
+})
 
 
 st.markdown("---")
