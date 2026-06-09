1f956ce5067a98c783ca5822febc80642163e54d
fcamara30-byte <fcamara30@gmail.com>
2026-06-09 10:53:42 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -1067,7 +1067,7 @@ von = math.sqrt(sigma**2 + 3*tau**2)
 
 uso = von / YS * 100
 
-
+st.write(f"N crítica (N): {N_crit:.0f}" if N_crit else "N crítica: N/A")
 
 
 
@@ -1077,7 +1077,7 @@ uso = von / YS * 100
 # RESULTADOS + GRAFICO
 # =========================
 c1, c2, c3, c6 = st.columns(4)
-st.write(f"N crítica (N): {N_crit:.0f}" if N_crit else "N crítica: N/A")
+
 if md_min is not None:
     st.markdown(
         f"<div style='text-align:right;'>👉 Zona crítica: {md_min:.0f} – {md_max:.0f} m</div>",
