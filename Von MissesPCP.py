1af4a47fbbad4f7910d445b6b6da7788855b9fb6
fcamara30-byte <fcamara30@gmail.com>
2026-06-09 09:03:52 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -1158,12 +1158,17 @@ else:
 # =========================
 c1, c2, c3, c6 = st.columns(4)
 
-st.markdown(
-    f"<div style='text-align:right;'>👉 Zona crítica: {md_min:.0f} – {md_max:.0f} m</div>"
-    if md_min is not None:
-    else "<div style='text-align:right;'>👉 No aplica (pozo vertical)</div>",
-    unsafe_allow_html=True
-)
+if md_min is not None:
+    st.markdown(
+        f"<div style='text-align:right;'>👉 Zona crítica: {md_min:.0f} – {md_max:.0f} m</div>",
+        unsafe_allow_html=True
+    )
+else:
+    st.markdown(
+        "<div style='text-align:right;'>👉 No aplica (pozo vertical)</div>",
+        unsafe_allow_html=True
+    )
+
 
 
 with c1:
