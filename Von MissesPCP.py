13ada2a60bb13d2c0bb046073b3a823e433f9c45
fcamara30-byte <fcamara30@gmail.com>
2026-06-09 21:24:20 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -898,7 +898,7 @@ for _, row in df.iterrows():
 
     elif inc_val > 30:
         colores.append("orange")
-        rec.append("Inc. cent/liner")
+        rec.append("Inc.liner")
 
     elif dls_val <= 3:
         colores.append("yellow")
