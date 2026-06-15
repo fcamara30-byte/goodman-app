ceee36c0187c0fe0d0b50dbdde95f49934389817
fcamara30-byte <fcamara30@gmail.com>
2026-06-15 10:26:33 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -1259,6 +1259,7 @@ with colS:
 
     # gráfico
 with colG:
+        st.markdown("##### Well Trajectory")
         if len(df) > 1:
             fig = plt.figure(figsize=(4,6))
             ax = fig.add_subplot(111, projection='3d')
