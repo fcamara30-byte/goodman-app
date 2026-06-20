d1e0a1acf28235016709cd6bfe3fcc44d98bf1ce
fcamara30-byte <fcamara30@gmail.com>
2026-06-20 19:28:05 -0300

    Update Von MissesPCP.py

--- Von MissesPCP.py
+++ Von MissesPCP.py
@@ -1310,7 +1310,7 @@ with colG:
                 df["Y"].iloc[i:i+2],
                 df["Z"].iloc[i:i+2],
                 color=colores[i],
-                linewidth=2
+                linewidth=1.5
             )
 
         # ✅ ESCALA ORIGINAL (NO TOCAR)
